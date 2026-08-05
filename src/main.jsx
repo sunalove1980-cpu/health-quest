01b195231aa85812afae40b4fab443e92673f3bb
sunalove1980 <sunalove1980@users.noreply.github.com>
2026-08-05 12:10:54 +0900

    파스텔 테마와 전날 모험 선택 흐름 추가

--- src/main.jsx
+++ src/main.jsx
@@ -1,2 +1,2 @@
-import React from 'react';import{createRoot}from'react-dom/client';import App from './App';import './styles.css';
+import React from 'react';import{createRoot}from'react-dom/client';import'@fontsource/pretendard/400.css';import'@fontsource/pretendard/600.css';import'@fontsource/pretendard/700.css';import'@fontsource/pretendard/800.css';import App from './App';import './styles.css';
 createRoot(document.getElementById('root')).render(<React.StrictMode><App/></React.StrictMode>);
